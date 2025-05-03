694c718148f222c800ac500ce360ad8f1feb4d68
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2025-05-03 20:45:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -560,7 +560,7 @@ def main():
     
     updater.start_polling()
     updater.job_queue.run_once(restart_active_quizzes, 0)
-    updater.job_queue.run_daily(reset_ignored_chats, time=time(0, 0))
+    updater.job_queue.run_daily(reset_active_chats, time=time(0, 0))
 
     updater.idle()
 
